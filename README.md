5a9d82b56f5afce99d17f115b1a4548cf994370b
Shubham Srivastava <cashivamsms@gmail.com>
2021-04-26 07:42:44 +0530

    adding commits

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # blog
+800540277562-iguicmero4rheqp4ldearitm0f8jbsn9.apps.googleusercontent.com
+
+
+0_nzv_8U5gZTPeKDZ27AdUm3
--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
+
 
 const app = express();
 
@@ -13,6 +15,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user',  userRoutes)
 
 const CONNECTION_URL = "mongodb+srv://admin:Shubham268@cluster0.r7fr0.mongodb.net/Blog?retryWrites=true&w=majority";
 const PORT = process.env.PORT|| 5000;
